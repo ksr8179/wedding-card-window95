3bedb6d52e7847fec476305a68fb7abcb5b0b05b
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-06-29 16:30:32 +0900

    TMAP 연동

--- app/composables/useTMap.ts
+++ app/composables/useTMap.ts
@@ -3,7 +3,7 @@ export const useTMap = () => {
 
     const startTmapNavigation = async (destination : {name: string, x:number, y:number}) => {
         const appKey = config.public.TmapAppKey;
-
+        console.log(appKey);
         // 응답 데이터를 변수에 담지 않고 호출만 실행
         await $fetch('https://apis.openapi.sk.com/tmap/app/routes', {
             method: 'GET',
