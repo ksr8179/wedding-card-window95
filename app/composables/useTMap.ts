a7ecc9c1c0aa08a04b19956b087583c0fe5697fc
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-06-30 08:04:46 +0900

    TMAP 연동 수정

--- app/composables/useTMap.ts
+++ app/composables/useTMap.ts
@@ -3,14 +3,12 @@ export const useTMap = () => {
 
     const startTmapNavigation = async (destination : {name: string, x:number, y:number}) => {
         const appKey = config.public.TmapAppKey;
-        const data = await $fetch('/api/tmap', {
-            query: {
-                appKey : appKey,
-                goalname : destination.name,
-                goalx : destination.x,
-                goaly : destination.y
-            }
-        });
+
+        window.location.href = 'https://apis.openapi.sk.com/tmap/app/routes' 
+                             + '&appKey=' + appKey
+                             + '&goalname=' + destination.name
+                             + '&goalx=' + destination.x
+                             + '&goaly=' + destination.y;
     };
 
     return {
