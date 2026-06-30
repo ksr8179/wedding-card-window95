41016ee8ba15b6375cc74705c6b9f95420d25b94
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-06-30 16:09:20 +0900

    카카오톡 공유하기 수정

--- app/composables/useKakaoShare.ts
+++ app/composables/useKakaoShare.ts
@@ -9,8 +9,7 @@ export const useKakaoShare = () => {
       const kakao = await loadKakaoSDK();
 
       Kakao.Share.sendCustom({
-        templateId: 134868,  // 공유 할 메시지 템플릿 ID 입력
-        templateArgs: true,  // 메시지 템플릿에 설정한 사용자 인자 JSON 입력
+        templateId: 134868  // 공유 할 메시지 템플릿 ID 입력
       });
     } catch (error) {
       console.error('공유하기 실행 실패:', error);
