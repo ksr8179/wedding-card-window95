b29ed8e8913681d17e19ab8b0d4ded07e2f049e5
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-07-02 16:07:03 +0900

    supabase 연결 및 이미지 가져오기

--- server/api/gallery.ts
+++ server/api/gallery.ts
@@ -3,7 +3,6 @@ import { serverSupabaseClient } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
   try {
-    // 1. 여기서 터지는지 확인
     const client = await serverSupabaseClient(event)
     const { gubun } = getQuery(event)
     
@@ -18,14 +17,13 @@ export default defineEventHandler(async (event) => {
 
     dbQuery = dbQuery.order('id', { ascending: true }) // 정렬
 
-    // 2. 여기서 터지는지 확인
     const { data, error } = await dbQuery
     
     if (error) {
       return { source: 'Supabase DB Error', details: error }
     }
     
-    return { source: 'Success', data }
+    return data
   } catch (err: any) {
     // 에러가 나면 숨기지 말고 브라우저에 에러 메시지를 JSON으로 뱉어내라!
     return { 
