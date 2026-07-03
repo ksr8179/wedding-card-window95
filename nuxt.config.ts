ae03ff14bb8e5df3cb31685c8921a922cf49d0b4
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-07-03 15:55:06 +0900

    schedule 지도 재수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   },
   image: {
     provider: 'ipx',
-    domains: process.env.SUPABASE_URL, // 본인의 supabase 프로젝트 도메인 등록
+    domains: [process.env.SUPABASE_URL || 'localhost'], // 본인의 supabase 프로젝트 도메인 등록
   },
   // nitro: {
   //   preset: 'github_pages',
