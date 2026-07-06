1d322d30b54b681707fe52fe49b737fe0d6d5120
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-07-06 15:30:05 +0900

    사운드 실행 방식 수정

--- app/composables/useSound.ts
+++ app/composables/useSound.ts
@@ -13,11 +13,12 @@ export const useSound = (audioUrl) => {
 
   // 1. 오디오 재생 함수 (사용자 상호작용으로 실행되므로 브라우저 환경이 보장됨)
   const play = () => {
-    audio.value.play().catch(err => {
-      console.error("오디오 재생 실패:", err)
-    })
+    if (audio) {
+      audio.currentTime = 0
+      audio.play()
+    }
   }
-
+  
   // 2. 오디오 일시정지 함수
   const pause = () => {
     if (audio.value) {
