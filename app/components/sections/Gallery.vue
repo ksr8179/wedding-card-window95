233d5988ed36ff344bf42e9fa9f0c77370c9bb3a
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-07-07 10:15:51 +0900

    object-cover로 수정

--- app/components/sections/Gallery.vue
+++ app/components/sections/Gallery.vue
@@ -28,7 +28,7 @@
         >
           <!-- [수정] v-show와 @load를 추가하여 렌더링이 끝난 시점에만 노출 -->
           <NuxtImg 
-            class="w-full h-full object-contain" 
+            class="w-full h-full object-cover" 
             :src="runtimeConfig.public.supabaseUrl + config.ImgPath + img.url +'?t=' + imageTimestamp"
             loading="eager" 
             quality="80"
