a9051e6142f56f14c0e1e97be819b9ed1752bdf5
김성래 그룹웨어 지원 Helpdesk <DT075595@mobis-partners.com>
2026-08-03 09:16:25 +0900

    위치 정보 및 일자 수정

--- app/config/wedding.config.ts
+++ app/config/wedding.config.ts
@@ -4,7 +4,7 @@ export const weddingConfig = {
     hero: {
       title: "WEDDING INVITATION",
       names: ["김성래", "장혜민"],
-      date: "202x.xx.xx",
+      date: "2027.01.30",
     },
     // 2. 초대 문구
     message: {
@@ -13,22 +13,22 @@ export const weddingConfig = {
     },
     // 3. 날짜 및 장소
     schedule: {
-      date: "202x년 x월 xx일 (토)",
-      time: "오후 2시",
-      location: "xx시 xx구 xx로 웨딩홀 3층",
-      address: "xx시 xx구 xx로 123",
-      lat : 35.1782,
-      lng : 128.1365
+      date: "2027년 1월 30일 (토)",
+      time: "오후 1시 20분",
+      location: "MBC컨벤션진주 1관",
+      address: "경상남도 진주시 충무 공동 415",
+      lat : 35.1828367,
+      lng : 128.1355786
     },
     // 4. 계좌 정보
     accounts: {
-      groom: { name: "김성래", bank: "국민은행", number: "123-456-7890" },
+      groom: { name: "김성래", bank: "부산은행", number: "123-456-7890" },
       bride: { name: "장혜민", bank: "신한은행", number: "098-765-4321" },
     },
     // 5. 연락처
     contacts: {
-      groom: "010-1234-5678",
-      bride: "010-9876-5432",
+      groom: "010-7244-8179",
+      bride: "010-9116-7592",
     },
     ImgPath : '/storage/v1/object/public/uploads/',
     mouseSound : 'data:audio/wav;base64,UklGRpYDAABXQVZFZm10IBAAAAABAAEAESsAACJWAAACABAAZGF0YXIDAAAG3R0TetKbPS1LPMtoK/pNTPGhOP9/KLY+PcnVfuFy71rCEDXnKrqUqQV0Fn/1yv+q9gu8jSGKD3PuwhDi4Giv+hLADsnFlxilBEPE1An6H2Xaed7OMsjrnyFqEMjW7PDjEFryruMJH8cFu+gvFawS/7w+GhkVDMreFL0iO/pS27JJcRP/7f78eQeG5g33EPoP7B36dRl7AQH4I/v4GGwE/gdBEl0Vtuqp//wN9/U88XQJcwQv3ULo6heACWvl+Ap6CbUD2ep0+VwXkvte9IkI7RES8Lv9uCJz9FX8PfHwC0gP6vzG+3sMUQasAUv3RBon+f37QAkWD2r/2/mDDHgGTRMb/Sr0SwRoAIX/qwGDAUkBxfx9BOUCpQr7B9H38/ku/7YI4/9z+Gr9zgFyBJf/OgJf/2kDuQF3/X4ARgYWAwb6f/52ABL9UATAAQz28f/lAe4DLP+i/PACqf6wArX6Kv6r+LcAiANmBNb6T/lg+cz+qQR2Aaz6Ef50+0f+WgMV/Yr9Iv2D/Y4BLgLs/nf8Z/1iAdkBoAXJBK3+zv6i/4kDkwKp/3T/m/yS/O8AowEoAyL+Zf7A/zf+3AEmANAATAGY/m38Q/9bAecBSgAFAF7/UgEvAOT/SQGFAbP/EwBj/SP+tf5yAeQASQOa/xQAUf24/T//bQEsAiYAPgD/ ANz/f/80/2v+WwCj/5IBoQDt/9n++f4lABH/7f+IADwAUwHTASMAiwCb//7++f6g/sb/a/87AKQABQHIAJD/7/+q/+n/jP8e/3f/R/9TAOP/QACbAAcAbP/I/y4A1P9H/yYA2//6/7L/2f8yAFAAkAAkAJQA7AD8ABMAAgBxAOj/AwDC/7z/Vf9a/6b/4//9/6//rf+q/xgAWADo/zwAoAAnAE8AVQBmAC4AMgBvAEoAGQD9/wwAFAD3/00A2v8hAPb/8P/t/9D/0f8CAKj/EwDt/97/xP/p/9v/3P/4/8T/GQCk/+v/xv/g/xkAtf/p/xwAx//l/+v/6P/r/+f/BADl/9v/HwD9/ygADAATAPH/IAAEAP3/MAAVACAAOgAKABUAKQAMAO3/6P/+//7/6P/t/+T/7P/t//H/BgDx//n/AAAcAAkAGQAXAP7/FQD5/woAIgD4/wAABwD8/9j/6//6/+j/8P8AAAoADgAJACsA9/8='
